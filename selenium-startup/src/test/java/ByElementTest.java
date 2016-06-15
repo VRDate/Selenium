2a7babf5d526da6da102c78efd580ac0dd6d50bb
caiyuan <caiyuan@live.com>
2016-06-15 10:20:36 +0800

    selenium-startup

--- selenium-startup/src/test/java/ByElementTest.java
+++ selenium-startup/src/test/java/ByElementTest.java
@@ -1,10 +1,14 @@
+import com.google.common.base.Function;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -65,6 +69,49 @@ public class ByElementTest {
         driver.navigate().to("file:///Users/YUAN/Project/Selenium/selenium-startup/src/test/resources/test.html");
         WebElement el = driver.findElement(By.tagName("p"));
         assertEquals(el.getText(), "Hello from JavaScript!");
+        driver.quit();
+    }
+
+    /**
+     * FluentWait
+     */
+    @Test
+    public void t4() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        // Waiting 30 seconds for an element to be present on the page, checking for its presence once every 5 seconds.
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(5, SECONDS)
+                .pollingEvery(10, SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        driver.get("http://cn.bing.com");
+
+        WebElement leftButton = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.id("sh_lt"));
+            }
+        });
+
+        WebElement rightButton = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.id("sh_rt"));
+            }
+        });
+
+
+        Actions builder = new Actions(driver);
+
+        Thread.sleep(5000);
+        builder.click(leftButton).perform();
+        Thread.sleep(5000);
+        builder.click(rightButton).perform();
+        Thread.sleep(5000);
+        builder.click(leftButton).perform();
+        Thread.sleep(5000);
+        builder.click(rightButton).perform();
+        Thread.sleep(5000);
+
         driver.quit();
     }
 
